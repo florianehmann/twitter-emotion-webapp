8e4b8d82d13b1ea6012e17362784c3d7c0e7b24a
Florian Ehmann <kontakt@florianehmann.de>
2023-12-05 18:06:56 +0100

    Add User Classification Field

--- app/base/routes.py
+++ app/base/routes.py
@@ -6,7 +6,7 @@ from app import db
 from app.base import bp
 from app.base.forms import QueryForm
 from app.inference import query_model, ModelLoadingException, ModelQueryException
-from app.models import UserRequest
+from app.models import EMOTION_INDEX, UserRequest
 
 
 @bp.route('/', methods=['GET', 'POST'])
@@ -32,12 +32,13 @@ def process_user_request(form, common_kwargs):
 
     render = None
     tweet_text = form.tweet_text.data
+    user_classification = form.user_classification.data
     user_ip = request.headers.get('<X-Forwarded-For>') or 'unavailable'
 
     try:
         classifications = query_model(tweet_text)
         log_user_request(tweet_text, user_ip)
-        store_request_in_database(tweet_text, user_ip)
+        store_request_in_database(tweet_text, user_ip, user_classification)
         render = render_result(tweet_text, classifications, common_kwargs)
 
     except ModelQueryException as e:
@@ -51,9 +52,10 @@ def process_user_request(form, common_kwargs):
     return render
 
 
-def store_request_in_database(tweet_text, user_ip):
+def store_request_in_database(tweet_text, user_ip, user_classification: str):
     # pylint: disable=missing-function-docstring
-    user_request = UserRequest(tweet_text=tweet_text, user_ip=user_ip)
+    class_index = EMOTION_INDEX[user_classification] if user_classification in EMOTION_INDEX else None
+    user_request = UserRequest(tweet_text=tweet_text, user_ip=user_ip, user_classification=class_index)
     db.session.add(user_request)
     db.session.commit()
 
--- app/models.py
+++ app/models.py
@@ -2,11 +2,22 @@
 
 from datetime import datetime, timezone
 from textwrap import shorten
+from typing import Optional
 
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 
 from app import db
+
+
+EMOTION_INDEX = {
+    'sadness': 0,
+    'joy': 1,
+    'love': 2,
+    'anger': 3,
+    'fear': 4,
+    'surprise': 5,
+}
 
 
 # pylint: disable=too-few-public-methods
@@ -16,6 +27,7 @@ class UserRequest(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     tweet_text: so.Mapped[str] = so.mapped_column(sa.String(256))
     user_ip: so.Mapped[str] = so.mapped_column(sa.String(16))
+    user_classification: so.Mapped[Optional[int]] = so.mapped_column(sa.Integer)
     timestamp: so.Mapped[datetime] = so.mapped_column(index=True, default=lambda: datetime.now(timezone.utc))
 
     def __repr__(self):
